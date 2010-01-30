f5271787d4d69345e5b100a62858bf4eaa186d95
Michal Kwiatkowski <constant.beta@gmail.com>
2010-01-30 13:38:14 +0100

    Added a comment about communication between UI and rendering threads.

--- src/eti/domino/OpenGLView.java
+++ src/eti/domino/OpenGLView.java
@@ -5,20 +5,27 @@ import android.opengl.GLSurfaceView;
 import android.view.MotionEvent;
 
 public class OpenGLView extends GLSurfaceView {
-    DominoRenderer dominoRenderer;
-    
-    public OpenGLView(Context context) {
-        super(context);
-        dominoRenderer = new DominoRenderer(context);
-        setEGLConfigChooser(false);
-        setRenderer(dominoRenderer);
-    }
+	DominoRenderer dominoRenderer;
 
-    public boolean onTouchEvent(final MotionEvent event) {
-        queueEvent(new Runnable(){
-            public void run() {
-                dominoRenderer.touch(event.getX(), event.getY());
-            }});
-        return true;
-    }
+	public OpenGLView(Context context) {
+		super(context);
+		dominoRenderer = new DominoRenderer(context);
+		setEGLConfigChooser(false);
+		setRenderer(dominoRenderer);
+	}
+
+	// http://developer.android.com/reference/android/opengl/GLSurfaceView.Renderer.html
+	//
+	// The renderer will be called on a separate thread, so that rendering
+	// performance is decoupled from the UI thread. Clients typically need to
+	// communicate with the renderer from the UI thread, because that's where
+	// input events are received.
+	public boolean onTouchEvent(final MotionEvent event) {
+		queueEvent(new Runnable() {
+			public void run() {
+				dominoRenderer.touch(event.getX(), event.getY());
+			}
+		});
+		return true;
+	}
 }
